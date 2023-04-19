c76dc0fd82a7f63a14d9bffdb804ca24e019b0a0
erickma1 <110662611+erickma1@users.noreply.github.com>
2023-04-19 17:58:39 +0000

    changed home page
    there was an error

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'spa#index'
   get 'app', to: 'spa#index'
   get 'app/*path', to: 'spa#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
